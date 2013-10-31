ab8036eddad2902040164b1ff6b337f55ace3f6a
J.A. NachE <nache.nache@gmail.com>
2013-10-31 18:18:41 +0100

    Add image creation into makeISO script. uploading img test

--- util/makeISO.sh
+++ util/makeISO.sh
@@ -7,4 +7,7 @@
 mkdir -p iso/boot/grub 2>/dev/null
 cp -rf knOSe iso/boot/
 echo -e "set timeout=3\nset default=0\nmenuentry 'nOSe ALPHA'{\nmultiboot /boot/knOSe\nboot\n}" >iso/boot/grub/grub.cfg
+echo "Create iso"
 grub-mkrescue -o nOSe_test.iso iso
+echo "Also create image"
+grub-mkimage -p iso/boot -O i386-pc -o nOSe_test.img multiboot
