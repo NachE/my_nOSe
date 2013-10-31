c1f1c2f6e6b0d4963daa324990cca015976bec68
J.A. NachE <nache.nache@gmail.com>
2013-10-31 18:34:27 +0100

    change way to make img, update test img

--- util/makeISO.sh
+++ util/makeISO.sh
@@ -6,8 +6,19 @@
 
 mkdir -p iso/boot/grub 2>/dev/null
 cp -rf knOSe iso/boot/
+cp /boot/grub/stage1 /boot/grub/stage2 iso/boot/grub/
 echo -e "set timeout=3\nset default=0\nmenuentry 'nOSe ALPHA'{\nmultiboot /boot/knOSe\nboot\n}" >iso/boot/grub/grub.cfg
 echo "Create iso"
 grub-mkrescue -o nOSe_test.iso iso
+
 echo "Also create image"
-grub-mkimage -p iso/boot -O i386-pc -o nOSe_test.img multiboot
+dd if=/dev/zero of=nOSe_test.img bs=1024 count=3440
+losetup /dev/loop1 nOSe_test.img
+mkfs.ext2 /dev/loop1
+mkdir mnt
+mount -o loop /dev/loop1 mnt/
+cp -r iso/* mnt/
+grub-install --boot-directory=mnt/boot/ /dev/loop1
+umount mnt/
+losetup -d /dev/loop1
+
