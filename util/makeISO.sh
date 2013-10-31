3a29683512f040e37de3c7a3cc7751b716bceeb7
J.A. NachE <nache.nache@gmail.com>
2013-10-31 16:56:23 +0100

    add script to gen bootable iso and include iso test

---
+++ util/makeISO.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Documentation at: 
+# http://www.gnu.org/software/grub/manual/html_node/Making-a-GRUB-bootable-CD_002dROM.html
+# http://www.gnu.org/software/grub/manual/html_node/Simple-configuration.html#Simple-configuration
+# http://www.gnu.org/software/grub/manual/html_node/Multi_002dboot-manual-config.html
+
+mkdir -p iso/boot/grub 2>/dev/null
+cp -rf knOSe iso/boot/
+echo -e "set timeout=3\nset default=0\nmenuentry 'nOSe ALPHA'{\nmultiboot /boot/knOSe\nboot\n}" >iso/boot/grub/grub.cfg
+grub-mkrescue -o nOSe_test.iso iso
