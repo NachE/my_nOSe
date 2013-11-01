c6759aaa7c04761a361a68a9fc98ca1c6463a64e
NachE <nache.nache@gmail.com>
2013-11-01 21:19:06 +0100

    repos some things, using global pos var, fix syntax error

--- main/kmain.c
+++ main/kmain.c
@@ -3,56 +3,6 @@
 
 void kmain()
 {
-
-/*
- 0xB8000 Char in position 0
- 0xB8001 Options
- 0xB8002 Char in position 1
-*/
-
-unsigned short *cursor = (unsigned short *) 0xB8000;
-unsigned char str[] = "Loading Kernel nOSe...\0";
-unsigned int i;
-	for (i = 0; str[i]; i++){
-		/*0x0A = fg << 8 = bg*/
-		*cursor++ = str[i] | (0x0A << 8);
-	}
-
-	printk(" Test\0", cursor);
-
-/*
-4C L
-4F O
-41 A
-44 D
-49 I
-4E N
-47 G
-20 
-6E n
-4F O
-53 S
-65 e
-2E .
-2E .
-2E .
-*/
-/*
-*cursor++ = 0x4C | (0x0F << 9);
-*cursor++ = 0x4F | (0x0F << 9);
-*cursor++ = 0x41 | (0x0F << 9);
-*cursor++ = 0x44 | (0x0F << 9);
-*cursor++ = 0x49 | (0x0F << 9);
-*cursor++ = 0x4E | (0x0F << 9);
-*cursor++ = 0x47 | (0x0F << 9);
-*cursor++ = 0x20 | (0x0F << 9);
-*cursor++ = 0x6E | (0x0F << 9);
-*cursor++ = 0x4F | (0x0F << 9);
-*cursor++ = 0x53 | (0x0F << 9);
-*cursor++ = 0x65 | (0x0F << 9);
-*cursor++ = 0x2E | (0x0F << 9);
-*cursor++ = 0x2E | (0x0F << 9);
-*cursor++ = 0x2E | (0x0F << 9);
-*/
+	printk("Loading nOSe...\0");
 	for (;;);
 }
--- main/vga/vga.c
+++ main/vga/vga.c
@@ -1,6 +1,14 @@
 #include <nose/vga.h>
 
-unsigned short *printk(char str[], unsigned short *pos){
+/*
+ 0xB8000 Char in position 0
+ 0xB8001 Options
+ 0xB8002 Char in position 1
+*/
+
+unsigned short *pos = (unsigned short *) COLOR_VGA_ADDR;
+
+unsigned short *printk(char str[]){
 
 	unsigned int i;
 
@@ -8,6 +16,6 @@ unsigned short *printk(char str[], unsigned short *pos){
                 /*0x0A = fg << 8 = bg*/
                 *pos++ = str[i] | (0x0A << 8);
         }
-
+	
 	return pos;
 }
