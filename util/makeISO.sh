65cc630062bcd3d32567df2e0768d16a8ab93b20
J.A. Nache <nache.nache@gmail.com>
2013-11-06 23:32:13 +0100

    opensuse use grub2-** and qemu-system-** allow util recognice

--- util/makeISO.sh
+++ util/makeISO.sh
@@ -10,4 +10,6 @@ cp /boot/grub/stage1 /boot/grub/stage2 iso/boot/grub/
 cp /boot/x86_boot/grub/stage1 /boot/x86_boot/grub/stage2 iso/boot/grub/
 echo -e "set timeout=3\nset default=0\nmenuentry 'nOSe ALPHA'{\nmultiboot /boot/knOSe\nboot\n}" >iso/boot/grub/grub.cfg
 echo "Create iso"
-grub-mkrescue -o nOSe_test.iso iso
+grub-mkrescue -o nOSe_test.iso iso || grub2-mkrescue -o nOSe_test.iso iso
+# xorriso on opensuse is on libburnia-tools
+
--- util/startqemu.sh
+++ util/startqemu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-qemu -cdrom nOSe_test.iso -monitor stdio -m 32
+qemu -cdrom nOSe_test.iso -monitor stdio -m 32 || qemu-system-i386 -cdrom nOSe_test.iso -monitor stdio -m 32 
