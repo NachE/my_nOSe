dc7cefa3622f67f4b87d5d98675c3802b9b94423
J.A Nache <nache.nache@gmail.com>
2013-11-08 16:34:46 +0100

    no bios adress set parameter

--- util/startbochs.sh
+++ util/startbochs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bochs 'ata1-master: type=cdrom, path="./nOSe_test.iso", status=inserted' 'boot: cdrom' 'romimage: file=/usr/share/bochs/BIOS-bochs-latest, address=0xe0000' 'display_library: sdl'
+bochs 'ata1-master: type=cdrom, path="./nOSe_test.iso", status=inserted' 'boot: cdrom' 'romimage: file=/usr/share/bochs/BIOS-bochs-latest' 'display_library: sdl'
