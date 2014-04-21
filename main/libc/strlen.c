ff3fdd2e7f0caf17cc2f56e01dc557d0ea47fdcc
J.A Nache <nache.nache@gmail.com>
2014-04-21 18:03:54 +0200

    uploading strlen.c

---
+++ main/libc/strlen.c
@@ -0,0 +1,31 @@
+/*
+ *   strlen.c
+ *
+ *   This file is part of nOSe.
+ *
+ *   nOSe is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   nOSe is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with nOSe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+unsigned int strlen(char *str)
+{
+	unsigned int lenght = 0;
+	char *tmp = str;
+
+	while(*tmp++)
+		lenght++;
+	
+	return lenght;
+}
+
