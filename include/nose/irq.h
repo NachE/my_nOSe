e0d69982e00e6f0d1875616691a4919ef08f9eea
J.A Nache <nache.nache@gmail.com>
2014-04-24 18:44:58 +0200

    a prototype for install_irq

--- include/nose/irq.h
+++ include/nose/irq.h
@@ -10,6 +10,8 @@ typedef struct interrupts{
 void isr_kernel_debug(void);
 void isr_kernel(interrupts_t regs);
 void printINT(interrupts_t interrupt);
+void install_irq(unsigned int irq_num, void(*irq_func)(interrupts_t regs));
 extern void eoi_irq_a();
 extern void eoi_irq_b();
 extern char inportb1(unsigned short int port);
+
